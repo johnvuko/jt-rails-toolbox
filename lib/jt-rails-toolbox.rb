bdd81f1ab822b607b4ca2af368fb554711e3d8da
Jonathan Tribouharet <jonathan@siu-soon.com>
2016-11-28 16:06:11 +0100

    Fix bug - slack notifications

--- lib/jt-rails-toolbox.rb
+++ lib/jt-rails-toolbox.rb
@@ -115,7 +115,7 @@ module JTRailsToolbox
 
 				if @params['exception']['slack']
 					config.add_notifier :slack, {
-						webhook_url: params['exception']['slack']['webhook_url'],
+						webhook_url: @params['exception']['slack']['webhook_url'],
 					}
 				end
 
